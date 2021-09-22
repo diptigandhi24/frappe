c5abeca76cd7dc7628da32f876c7c3cf0ae0b085
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2021-09-22 20:24:10 +0530

    chore: move user api to frappe from bloomstack_core (#765)

--- frappe/utils/user.py
+++ frappe/utils/user.py
@@ -341,3 +341,11 @@ def get_users_with_role(role):
 		AND `tabUser`.`name`!='Administrator'
 		AND `tabHas Role`.`parent`=`tabUser`.`name`
 		AND `tabUser`.`enabled`=1""", role)]
+
+@frappe.whitelist()
+def set_roles():
+	"""overrides roles for a user"""
+	data = json.loads(frappe.request.data)
+	user = frappe.get_doc("User", data.get("uid"))
+	user.set("roles", list(set(d for d in user.get("roles") if d.role in data.get("roles"))))
+	user.add_roles(*data.get("roles"))
